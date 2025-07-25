d61347d95a2b4837fdb31fa480dbdb5341425934
denikilli <denikilli@protonmail.com>
2025-07-25 12:33:08 +0300

    file names

--- dani_homework/Inheritance/Inheritance_BankAccount_Task_6.py
+++
@@ -1,2 +0,0 @@
-# Bank Example Създай клас BankAccount с атрибут balance и метод check_balance(), който връща текущия баланс.
-# Създай клас SavingsAccount, който наследява BankAccount и има метод add_interest(rate), който увеличава баланса с процентното оскъпяване.
---
+++ dani_homework/Inheritance/Inheritance_Task_6_BankAccount.py
@@ -0,0 +1,6 @@
+# Bank Example
+# Създай клас BankAccount с атрибут balance и метод check_balance(),
+# който връща текущия баланс.
+# Създай клас SavingsAccount,
+# който наследява BankAccount и има метод add_interest(rate),
+# който увеличава баланса с процентното оскъпяване.
