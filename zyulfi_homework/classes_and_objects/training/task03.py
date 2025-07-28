a47b2d7027ee3446f6266c90976f4d1623078413
zyulfi <zyulfigugov@gmail.com>
2025-07-28 21:46:15 +0300

    Prints the type of vehicle started

---
+++ zyulfi_homework/classes_and_objects/training/task03.py
@@ -0,0 +1,14 @@
+# Задача 3
+# Създай клас Car с protected атрибут _engine. Добави метод start() и принтирай "Engine started".
+
+class Car:
+    def __init__(self, engine=str):
+        self._engine = engine
+
+    def start(self) -> None:
+        print(f"{self._engine} started")
+
+
+current_car = Car("Trabant")
+
+current_car.start()
