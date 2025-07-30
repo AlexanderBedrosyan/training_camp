06782a8e7554cc7cb9b6d9aad32d0157d53f737b
zyulfi <zyulfigugov@gmail.com>
2025-07-30 20:39:04 +0300

    Task 16 - music

---
+++ zyulfi_homework/classes_and_objects/inheritance_and_mixins_exercises/mixins/task_16_ElectricMixin.py
@@ -0,0 +1,6 @@
+# Задача 16
+# Създай клас ElectricMixin с метод plug_in() връщащ "Plugged in electric instrument"
+
+class ElectricMixin:
+    def plug_in(self):
+        return f"Plugged in electric instrument"
