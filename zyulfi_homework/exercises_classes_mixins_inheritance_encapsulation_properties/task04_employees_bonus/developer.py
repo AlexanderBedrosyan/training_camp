5785cfbc0a43f9197e23ca349c3a40f623008cb0
zyulfi <zyulfigugov@gmail.com>
2025-08-12 07:07:05 +0300

    Task 04 employes and managers

--- zyulfi_homework/exercises_classes_mixins_inheritance_encapsulation_properties/task04_employees_bonus/developer.py
+++ zyulfi_homework/exercises_classes_mixins_inheritance_encapsulation_properties/task04_employees_bonus/developer.py
@@ -1,1 +1,11 @@
 # Developer добавя level, метод code()
+from employee import Employee
+
+
+class Developer(Employee):
+    def __init__(self, name=str, salary=int, level=str):
+        super().__init__(name, salary)
+        self.level = level
+
+    def code(self) -> str:
+        return f"{self.name} is programing"
--- zyulfi_homework/exercises_classes_mixins_inheritance_encapsulation_properties/task04_employees_bonus/employee.py
+++ zyulfi_homework/exercises_classes_mixins_inheritance_encapsulation_properties/task04_employees_bonus/employee.py
@@ -1,1 +1,9 @@
 # Employee с name, _salary, методи get_salary()
+
+class Employee:
+    def __init__(self, name=str, salary=int):
+        self.name = name
+        self._salary = salary
+
+    def get_salary(self) -> None:
+        self._salary += 100
