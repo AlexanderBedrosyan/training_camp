91096c991068029a16248e71b175a1085a07f168
zyulfi <zyulfigugov@gmail.com>
2025-09-20 22:21:45 +0300

    Movie ratings

--- zyulfi_homework/exercises_hard_tasks/task07_movie_ratings/reviewer.py
+++ zyulfi_homework/exercises_hard_tasks/task07_movie_ratings/reviewer.py
@@ -1,1 +1,13 @@
 # Reviewer: име, метод rate_movie(movie, rating)
+
+class Reviewer:
+    def __init__(self, name_reviewer):
+        self.name_reviewer = name_reviewer
+
+    def rate_movie(self, movie=object, rating=int):
+        movie.add_rating(rating)
+
+
+
+
+
