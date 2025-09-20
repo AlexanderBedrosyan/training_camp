824465a9553151d3e7fc70e7f8c1a4f59c972227
zyulfi <zyulfigugov@gmail.com>
2025-09-20 23:05:55 +0300

    Fitnes tracker

--- zyulfi_homework/exercises_hard_tasks/task08_fitness_tracker/user.py
+++ zyulfi_homework/exercises_hard_tasks/task08_fitness_tracker/user.py
@@ -1,1 +1,13 @@
 # User: списък от активности, метод total_calories()
+from activity import Activity
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.list_of_activity: list[Activity] = []
+
+    def add_activity(self, curr_activity) -> None:
+        self.list_of_activity.append(curr_activity)
+
+    def total_calories(self) -> float:
+        return sum([curr_activity.calories for curr_activity in self.list_of_activity])
