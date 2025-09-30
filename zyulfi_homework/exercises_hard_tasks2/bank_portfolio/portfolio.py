beb414dd97b88f330abef9acebcecfff948c7ae6
zyulfi <zyulfigugov@gmail.com>
2025-09-30 05:42:00 +0300

    Bank Portfolio

--- zyulfi_homework/exercises_hard_tasks2/bank_portfolio/portfolio.py
+++ zyulfi_homework/exercises_hard_tasks2/bank_portfolio/portfolio.py
@@ -14,14 +14,15 @@ class Portfolio:
     def add_investment(self, curr_investment=object) -> None:
         self.list_of_investments.append(curr_investment)
 
+    def all_accounts(self) -> float:
+        return sum([curr_account.get_balance() for curr_account in self.list_of_accounts])
+
     def total_value(self) -> float:
-        all_accounts = sum([curr_account.get_balance() for curr_account in self.list_of_accounts])
         all_investment = sum([curr_investment.get_investment() for curr_investment in self.list_of_investments])
-        return all_accounts + all_investment
+        return  self.all_accounts() + all_investment
 
     def projected_value(self, years):
-        all_accounts = sum([curr_account.get_balance() for curr_account in self.list_of_accounts])
         all_investment = sum([curr_investment.calculate_return(years) for curr_investment in self.list_of_investments])
-        return all_accounts + all_investment
+        return self.all_accounts() + all_investment
 
 
