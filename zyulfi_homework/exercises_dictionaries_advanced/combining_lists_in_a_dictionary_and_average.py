716d94b737bdf94e059e74f72ee34fdbb0c254b6
zyulfi <zyulfigugov@gmail.com>
2025-10-12 22:32:34 +0300

    Задача 15: Комбиниране на списъци в речник и средна стойност

--- zyulfi_homework/exercises_dictionaries_advanced/combining_lists_in_a_dictionary_and_average.py
+++ zyulfi_homework/exercises_dictionaries_advanced/combining_lists_in_a_dictionary_and_average.py
@@ -1,8 +1,21 @@
-import unittest
+# Задача 15: Комбиниране на списъци в речник и средна стойност
+# Условие:
+# combine_to_dict(names, values) прави {name: value} и връща речника + средната стойност на values (round 2).
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
-if __name__ == '__main__':
-    unittest.main()
+def combine_to_dict(names, values):
+    # new_dict = dict(zip(names, values))
+    # return [new_dict, sum(new_dict.values()) / len(new_dict)]
+    if len(names) != len(values):
+        return "Wrong"
+
+    curr_dict = {}
+    for i in range(len(names)):
+        curr_dict[names[i]] = values[i]
+    return curr_dict, sum(curr_dict.values()) / len(curr_dict)
+
+students = ["Иван","Мария","Петър"]
+scores = [85,92,78]
+print(combine_to_dict(students, scores))
+# Очакван изход:
+# ({'Иван':85,'Мария':92,'Петър':78}, 85.0)
