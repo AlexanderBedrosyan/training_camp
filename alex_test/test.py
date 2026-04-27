f93e7baa321742ff167bf1bfce95a199d860bb91
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2026-04-27 12:00:00 +0300

    Last update

--- alex_test/test.py
+++ alex_test/test.py
@@ -1,6 +1,13 @@
-current_dict = {
-    "name": "Sasho",
-    "age": 14
-}
+list_of_names = ["Gosho", "Tosho", "Pesho", "Stamat"]
 
-print(current_dict["name"])
+print("====================================================")
+print(list_of_names)
+
+print(f"{', '.join(list_of_names)}") # Gosho, Tosho, Pesho, Stamat
+
+print(f"{'\n'.join(list_of_names)}")
+
+
+current_names = "Pesho Gosho Stamat"
+print(', '.join(current_names.split()))  # Pesho, Gosho, Stamat
+print(type(current_names.split()))
---
+++ db_tests/db_tests.py
@@ -0,0 +1,6 @@
+current_boolean = ""
+
+if not current_boolean:
+    print("The value is empty or False.")
+else:
+    print("The value is NOT empty and is True. ---- " + current_boolean)
